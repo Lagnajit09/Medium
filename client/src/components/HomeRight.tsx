704e5bf193f71d18a522dcb787a6c6b258ca5635
Lm09 <123.moharana@gmail.com>
2024-06-02 20:04:26 +0530

    add: FAQ FE

--- client/src/components/HomeRight.tsx
+++ client/src/components/HomeRight.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react'
 import { userTopicsAtom } from '../store/userAtom'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { fetchRecommendedTopics } from '../handlers/userHandlers';
-import { Skeleton } from '@mui/material';
-import { HiArrowRight } from "react-icons/hi2";
 import { useNavigate } from 'react-router-dom';
 import { Recommended } from './Recommended';
-
+import Button from './Button';
+import { IoCloseOutline } from 'react-icons/io5';
 
 const HomeRight = () => {
 
     const userTopics = useRecoilValue(userTopicsAtom)
     const [recommended, setRecommended] = useState([1,2,3,4,5,6,7])
     const [loading, setLoading] = useState(true)
+    const [showFAQ, setShowFAQ] = useState(true)
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -55,7 +55,8 @@ const HomeRight = () => {
     if(!recommended) return;
 
   return (
-    <div id='recommended-topics' className='flex w-[22%] border-l-2 px-5 fixed right-32 h-[100vh]'>
+    <div id='recommended-topics' className='flex flex-col w-[22%] border-l-2 px-5 fixed right-32 h-[100vh]'>
+        {/* RECOMMENDED */}
         <div className='mt-10'>
             <p className=' font-semibold text-base text-gray-700'>Recommended Topics</p>
             <div className=' flex flex-wrap items-center justify-start mt-3'>
@@ -67,6 +68,28 @@ const HomeRight = () => {
                 <p>See more topics</p>
             </div>
         </div>
+        {/* FAQ AND WRITING */}
+        {showFAQ && <div className='flex p-4 bg-blue-200 rounded-sm mt-8 justify-between'>
+            <div className=" w-[85%] flex flex-col gap-3">
+                <h4 className=' text-sm font-semibold text-black'>Writing on Medium</h4>
+                <div className='text-sm text-gray-900 flex flex-col gap-2'>
+                    <p className=' cursor-pointer hover:text-black'>New writer FAQ</p>
+                    <p className=' cursor-pointer hover:text-black'>Expert writing advice</p>
+                    <p className=' cursor-pointer hover:text-black'>Grow your redearship</p>
+                </div>
+
+                <Button 
+                    title='Start Writing' 
+                    onClick={() => navigate('/new-story', {
+                        state: {title: '', data: {}}
+                    })}
+                    buttonStyles=' w-[50%] py-2 bg-gray-800 text-white text-xs rounded-full font-normal mt-2'   
+                />
+            </div>
+            <div className=' h-[14%] mx-auto text-xl cursor-pointer' onClick={() => setShowFAQ(false)}>
+                <IoCloseOutline />
+            </div>
+        </div>}
     </div>
   )
 }
