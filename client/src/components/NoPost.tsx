2a8ae0f7c965bb258e15bf4edd1eae3dd61c23eb
Lm09 <123.moharana@gmail.com>
2024-07-02 21:42:10 +0530

    add: noPosts component

--- client/src/components/NoPost.tsx
+++ client/src/components/NoPost.tsx
@@ -7,22 +7,22 @@ const NoPost = () => {
     const navigate = useNavigate()
   return (
     <div className=" m-auto md:ml-[15%] w-fit flex flex-col items-center gap-5 text-center">
-          <p className=' text-gray-500'>Currently no posts available for the followed topics!</p>
+          <p className=' text-gray-600 dark:text-gray-400'>Currently no posts available for the followed topics!</p>
           <Button 
             title='Be the first one to publish' 
             onClick={() => {navigate('/new-story', {
                 state: {title: '', data: {}}
             })}}
-            buttonStyles='bg-white border-2 border-gray-600 rounded-lg font-semibold text-sm text-gray-600'
+            buttonStyles='bg-white border-2 border-gray-600 rounded-lg font-semibold text-sm text-gray-600 dark:bg-transparent dark:text-gray-300 dark:border-gray-300'
             icon={<RxPencil2 style={{color: 'gray'}} />}
           />
           <Button 
             title='View all topics' 
             onClick={() => {navigate('/all-topics')}} 
-            buttonStyles=' md:hidden bg-white border-2 border-gray-600 rounded-lg font-semibold text-sm text-gray-600'
+            buttonStyles=' md:hidden bg-white border-2 border-gray-600 rounded-lg font-semibold text-sm text-gray-600 dark:bg-transparent dark:text-gray-300 dark:border-gray-300'
             icon={<PiPlusCircleThin style={{color: 'gray'}} />}
           />
-        </div>
+    </div>
   )
 }
 
--- client/src/components/TopicPosts/TopicPosts.tsx
+++ client/src/components/TopicPosts/TopicPosts.tsx
@@ -1,4 +1,5 @@
 import { postType } from "../../pages/topicPosts"
+import NoPost from "../NoPost"
 import TopicCard from "./TopicCard"
 
 interface PropsType {
@@ -10,9 +11,16 @@ const TopicBody = ({posts}:PropsType) => {
   return (
     <div className=" w-[98%] md:w-[90%] mx-auto flex flex-wrap gap-5">
 
-        {posts?.map((post, index) => (
+        {posts?.length ? (
+          posts?.map((post, index) => (
              <TopicCard post={post} key={index} />
-        ))}
+        ))
+        ) : (
+          <div className=" w-[80%] md:w-[43%] m-auto">
+            <NoPost />
+          </div>
+        )
+        }
     </div>
   )
 }
